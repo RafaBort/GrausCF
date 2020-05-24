f0da9385c6dc7d16b38d6fba19ade926bb4b6dc1
RafaBort <rafaelafonseca.bort2019ri@gmail.com>
2020-05-24 20:44:42 -0300

    exercício resolvido

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite alguma temperatura em graus celsius, e veja ela se transformar em fahrenheit...");
+            string celsius = Console.ReadLine();
+
+            Double C = Convert.ToDouble(celsius);
+            Double F = C * 1.8 + 32;
+
+            Console.WriteLine("A temperatura em Fahrenheit é" + " " + F);
+            
+
+        }
+    }
+}
